d1fd416911ecb86fe502ba68ee787794bb868dca
RamadhikaS <112864129+RamadhikaS@users.noreply.github.com>
2022-09-05 22:06:16 +0700

    Add files via upload

---
+++ Tugas Daspro 1 ( A11.2022.14116)/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NAMA : Ramadhika Surya Purmiadanu" << endl;
+    cout << "NIM : A11.2022.14116" << endl;
+    cout << "Alamat : Jln Pancakarya Blok 53/318" << endl;
+    cout << "Asal Sekolah : SMAN 14 SEMARANG" << endl;
+    return 0;
+}
