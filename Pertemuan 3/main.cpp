3fb4c7ff4ecf26fbd3f25efc8407b54a25c732fc
RamadhikaS <112864129+RamadhikaS@users.noreply.github.com>
2022-09-12 15:37:02 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    printf("welcome to mobile legends");
+
+    int a = 17;
+    //int = innterger
+
+    float h = 175.2;
+    //float = khusus desimal
+
+    char o ='L';
+    //char untuk karakter, dan hanya satu karakter
+
+    string z = "XXl";
+    //string untuk karakter yang bisa menampilkan lebih dari satu karakter
+
+    cout << endl;
+    cout << "umur paduka :" << a;
+    cout << endl;
+    //cout << endl; berfungsi sebagai pembuat barisan baru
+
+    cout << "tinggi badan paduka :" << h;
+    cout << "\nukuran baju paduka :" << z;
+    // \n berfungsi sebagai pembuat barisan baru selain cout ,, endl;
+
+    /*
+    slash bintang berfungsi sebagai catatan
+    selain dengan menggunakan //. Yang kelebihannya
+    dapat mencangkup lebih dari satu baris.
+    dengan cara di atas bari menggunakan slash bintang
+    sedangkan untuk di bawah baris menggunakan bintang slash
+    */
+
+    return 0;
+}
