35edbb7c25551838b6a1ba164a478372044ce4a2
RamadhikaS <112864129+RamadhikaS@users.noreply.github.com>
2022-09-19 18:01:26 +0700

    Add files via upload

---
+++ Pertemuan 5/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    float  a,b;
+    a = 3.14;
+
+    cout << " PROGRAM PERHITUNGAN LUAS LINGKARAN " << endl;
+
+    cout << " Berapa jari jari Lingkarannya? " ;
+    cin >> b;
+
+    cout << endl;
+
+    cout << " Jawabannya adalah = " << a*b*b << endl;
+
+    cout << endl;
+
+    cout << " PROGRAM PERHITUNGAN LUAS PERSEGI PANJANG " << endl;
+
+    float x,y;
+
+    cout << " Berapa tinggi Persegi Panjangnya? " ;
+    cin >> x;
+
+    cout << endl;
+
+    cout << " Berapa lebar Persegi Panjangnya? " ;
+    cin >> y;
+
+    cout << endl;
+
+    cout << " Jawabannya adalah = " << x*y << endl;
+    return 0;
+}
